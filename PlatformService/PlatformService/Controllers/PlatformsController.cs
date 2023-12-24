6ef9a2dbe93daf3bec2e8f4e882e4d916df23ed3
Schütz Máté <schutz.mate@gmail.com>
2023-12-24 16:34:15 +0100

    Add initial implementation

---
+++ PlatformService/PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+using PlatformService.SyncDataServices.Http;
+
+namespace PlatformService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    private readonly IPlatformRepo _platformRepo;
+    private readonly IMapper _mapper;
+    private readonly ICommandDataClient _commandDataClient;
+
+    public PlatformsController(
+        IPlatformRepo platformRepo,
+        IMapper mapper,
+        ICommandDataClient commandDataClient)
+    {
+        _platformRepo = platformRepo;
+        _mapper = mapper;
+        _commandDataClient = commandDataClient;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        Console.WriteLine("--> Getting Platforms...");
+
+        var platforms = _platformRepo.GetAllPlatforms();
+
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+    }
+
+    [HttpGet("{id}", Name = "GetPlatformById")]
+    public ActionResult<PlatformReadDto> GetPlatformById(int id)
+    {
+        Console.WriteLine("--> Getting Platform By Id...");
+
+        var platform = _platformRepo.GetPlatformById(id);
+
+        if (platform == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(_mapper.Map<PlatformReadDto>(platform));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PlatformReadDto>> CreatePlatform(PlatformCreateDto platformToCreate)
+    {
+        Console.WriteLine("--> Creating Platform...");
+
+        var platform = _mapper.Map<Platform>(platformToCreate);
+        _platformRepo.CreatePlatform(platform);
+        _platformRepo.SaveChanges();
+
+        var platformReadDto = _mapper.Map<PlatformReadDto>(platform);
+
+        try
+        {
+            await _commandDataClient.SendPlatformToCommand(platformReadDto);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"--> Could not send synchronously: {ex.Message}");
+        }
+
+        return CreatedAtRoute(nameof(GetPlatformById), new { platformReadDto.Id }, platformReadDto);
+    }
+}
---
+++ PlatformService/PlatformService/Data/PrepDb.cs
@@ -0,0 +1,34 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data;
+public static class PrepDb
+{
+    public static void PrepPopulation(IApplicationBuilder app)
+    {
+        using IServiceScope serviceScope = app.ApplicationServices.CreateScope();
+
+        SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+    }
+
+    private static void SeedData(AppDbContext? context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        if (!context.Platforms.Any())
+        {
+            Console.WriteLine("--> Seeding data...");
+
+            context.Platforms.AddRange(
+                new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                new Platform() { Name = "Java", Publisher = "JavaCorp", Cost = "200" },
+                new Platform() { Name = "Docker", Publisher = "Microsoft", Cost = "50" }
+                );
+
+            context.SaveChanges();
+        }
+        else
+        {
+            Console.WriteLine("--> We already have data, therefore we do not seed data.");
+        }
+    }
+}
